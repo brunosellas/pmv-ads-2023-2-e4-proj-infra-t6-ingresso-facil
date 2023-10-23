970ab31d3b68d577a92711483a162459cf563b2b
brunosellas <bruno_sellas@hotmail.com>
2023-10-23 18:38:02 -0300

    tentando iniciar o projeto, resolvendo erro de importação da página login

---
+++ dev/frontweb/ingrefacweb/src/Components/Component.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Component() {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+}
---
+++ dev/frontweb/ingrefacweb/src/Pages/Login.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Component from "../Components/Component.js";
+
+
+export default function login() {
+    return (
+        <Component></Component>
+    );
+}
